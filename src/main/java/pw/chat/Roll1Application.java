ce79076efdac2927ceddcba903a9141b0173733c
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2021-10-25 09:14:25 +0200

    Rename main class

--- src/main/java/pw/chat/Roll1Application.java
+++ src/main/java/pw/chat/Roll1Application.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Roll1Application extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Roll 1");
 
-        var loader = new FXMLLoader(HelloApplication.class.getResource("main-screen.fxml"));
+        var loader = new FXMLLoader(Roll1Application.class.getResource("main-screen.fxml"));
         var scene = new Scene(loader.load(), 600, 400);
         stage.setScene(scene);
         stage.show();
